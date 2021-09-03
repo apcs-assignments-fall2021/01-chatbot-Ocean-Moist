a1d085e935d3fcf1017383f898737bb874e9434c
Rohan <rohanzealous@gmail.com>
2021-09-02 19:09:12 -0700

    For some reason I have apcs 2020 and 2021 projects. I am going to commit to both. Uncomment lines 14-17 for proper functionality. The while loop messes up tests.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Add some code here!
-
-        scan.close();
+        System.out.println("Hey Whats ur name");
+        String name = scan.nextLine();
+        System.out.println("hello " + name + "\nare you a big baller");
+        String answer = scan.nextLine();
+        System.out.println(answer + "? I knew it\non a scale of 1-10 how cool are you");
+        int answer2 = Integer.parseInt(scan.nextLine());
+        System.out.println("Hmm I think you are a " + (answer2 - 1) + "\nIs the creator of this chatbot the best person ever?");
+        String answer3 = scan.nextLine();
+       /* while (!answer3.toLowerCase().contains("yes")) {
+            System.out.println("Wrong Answer! Try again");
+            answer3 = scan.nextLine();
+        }*/
     }
 }
